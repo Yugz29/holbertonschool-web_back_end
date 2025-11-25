52c099404825005d6db7f7bd15104945d1870890
Yugz29 <yannduzelier@gmail.com>
2025-11-25 19:22:00 +0100

    add: function floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""Module that defines a function to compute the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n as an integer."""
+    return math.floor(n)
