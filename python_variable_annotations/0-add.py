1c46e198ad653966f007d27b8f3e49043cf21578
Yugz29 <yannduzelier@gmail.com>
2025-11-25 18:55:58 +0100

    add: docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module that defines a funct to add two float numbs with type annotations."""
 def add(a: float, b: float) -> float:
     """Return the sum of two float numbers."""
     return a + b
