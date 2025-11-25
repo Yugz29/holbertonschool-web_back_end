44b0d46ae7c4c7bc55d6760ae624d676f4f3c187
Yugz29 <yannduzelier@gmail.com>
2025-11-25 22:13:18 +0100

    add: List[]

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module that defines a function to sum a list of floats."""
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of all the floats in input_list."""
     return sum(input_list)
