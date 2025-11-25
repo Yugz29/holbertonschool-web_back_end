019ca7b21c7965d656baaaffe7a0b10fcb7fa577
Yugz29 <yannduzelier@gmail.com>
2025-11-25 22:32:35 +0100

    add: function to create a key/value tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that defines a function to create a key-value tuple."""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple containing the string k and the square of v."""
+    return k, v * 2
