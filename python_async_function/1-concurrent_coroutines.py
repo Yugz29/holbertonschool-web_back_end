08f0b454809af1445a5e09556455cf882162a989
Yugz29 <yannduzelier@gmail.com>
2025-11-27 18:34:50 +0100

    add: asynchrone coroutine

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that demonstrate concurrent execution of multiple async coroutines"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Asynchronous coroutine that runs wait_random n times concurrently."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
