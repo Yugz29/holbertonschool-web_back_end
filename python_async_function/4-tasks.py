65738be67d15aacd5308dca5d5fb5e22c3f7c9f6
Yugz29 <yannduzelier@gmail.com>
2025-11-28 11:36:26 +0100

    add: function thaht execute task_wait_random n times

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for task_wait_n"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Execute task_wait_random n times concurrently."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
