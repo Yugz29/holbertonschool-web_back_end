e2233a74fc443625ddccc0d0803586c6444e31f9
Yugz29 <yannduzelier@gmail.com>
2025-11-28 14:42:53 +0100

    add: function to mesure the time

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that measures runtime of async comprehension"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Measures the runtime of async_comprehension function."""
+    start = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end = time.time()
+    return end - start
