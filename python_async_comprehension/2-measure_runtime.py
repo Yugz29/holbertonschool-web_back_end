f0e8746b6b1540c771e8be95ff7b048dfc2800b5
Yugz29 <yannduzelier@gmail.com>
2025-11-28 14:45:47 +0100

    add: -> float:

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Measures the runtime of async_comprehension function."""
     start = time.time()
     await asyncio.gather(*[async_comprehension() for _ in range(4)])
