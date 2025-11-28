5bcf2eab42111967a1aa8ee459e7acf6a5f541a1
Yugz29 <yannduzelier@gmail.com>
2025-11-28 13:14:29 +0100

    fix: random.random to random.uniform(0, 10)

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Async generator that yields 10 random numbers with a 1 sec delay."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random()
+        yield random.uniform(0, 10)
