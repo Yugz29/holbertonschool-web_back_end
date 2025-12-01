fbdb2c4ed43c761bfd1ba88de6b3f99d79d6d6f7
Yugz29 <yannduzelier@gmail.com>
2025-12-01 14:30:20 +0100

    remove: verification out of range

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,15 +33,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return the appropriate page of the dataset (list of lists)."""
-        assert isinstance(page, int)
-        assert page > 0
-        assert isinstance(page_size, int)
-        assert page_size > 0
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-
-        if start_index >= len(dataset):
-            return []
 
         return dataset[start_index:end_index]
