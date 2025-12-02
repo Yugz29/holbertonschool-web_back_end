3cb2c5e4c8ad6977ba4054a377d526d07d0bddd3
Yugz29 <yannduzelier@gmail.com>
2025-12-02 19:28:43 +0100

    fix: insert to insert_one

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,6 +8,6 @@ def insert_school(mongo_collection, **kwargs):
     function that inserts a new document in a collection
     and returns the new document's ID
     """
-    result = mongo_collection.insert(kwargs)
+    result = mongo_collection.insert_one(kwargs)
     new_id = result.inserted_id
     return new_id
