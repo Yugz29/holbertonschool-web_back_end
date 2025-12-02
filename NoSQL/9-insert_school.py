4e3a1fc6d9877f3f684ffacf59da8e16822e2624
Yugz29 <yannduzelier@gmail.com>
2025-12-02 19:27:26 +0100

    add: function that inserts and return ID

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Inserts a new document in a collection
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    function that inserts a new document in a collection
+    and returns the new document's ID
+    """
+    result = mongo_collection.insert(kwargs)
+    new_id = result.inserted_id
+    return new_id
