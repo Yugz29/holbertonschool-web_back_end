c20c9714aa0c6187dd9cb9e732469e74d0108845
Yugz29 <yannduzelier@gmail.com>
2025-12-15 21:48:39 +0100

    add:  function that return an array of objects

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+function getStudentsByLocation(list, city) {
+    if (!Array.isArray(list)) return [];
+    
+    return list.filter(student => student.location === city);
+}
+
+export default getStudentsByLocation;
