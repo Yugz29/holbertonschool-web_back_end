9c5f8b27a751c4bd15c0d9b0cb39b75a54e8b232
Yugz29 <yannduzelier@gmail.com>
2025-12-19 12:11:21 +0100

    add: function that collectively resolve all promises and log a msg

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    Promise.all([photoPromise, userPromise])
+        .then(([{ body }, { firstName, lastName}]) => {
+            console.log(body, firstName, lastName);
+        })
+        .catch(error => {
+            console.log('Signup system offline');
+        });
+}
