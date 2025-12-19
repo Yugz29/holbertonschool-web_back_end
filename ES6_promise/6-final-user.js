21f6743f63641fa36a7f0f26962a8f5c497580c7
Yugz29 <yannduzelier@gmail.com>
2025-12-19 14:08:03 +0100

    fix: name function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise.js'
 import uploadPhoto from './5-photo-reject.js'
 
-export default function handleProfileSingup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
     const userPromise = signUpUser(firstName, lastName);
     const photoPromise = uploadPhoto(fileName);
 
