188b8ebb977d6bad83526ec246ba3922f6bfdb45
Yugz29 <yannduzelier@gmail.com>
2026-01-05 11:45:02 +0100

    fix: add process.stdin.isTTY

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
+  
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
 });
 
 process.stdin.on('end', () => {
