809b5fc433c816e78339e4b02711b03bf005b5d3
Yugz29 <yannduzelier@gmail.com>
2026-01-05 15:57:37 +0100

    fix: ESlint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,7 +6,7 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
     const lines = data.split('\n').slice(1);
-    students = lines.filter(line => line !== '');
+    students = lines.filter((line) => line !== '');
     console.log(`Number of students: ${students.length}`);
   } catch (err) {
     throw new Error('Cannot load the database');
@@ -14,10 +14,10 @@ function countStudents(path) {
 
   const groups = {
     CS: [],
-    SWE: []
+    SWE: [],
   };
 
-  for (let i = 0; i < students.length; i++) {
+  for (let i = 0; i < students.length; i += 1) {
     const line = students[i];
     const parts = line.split(',');
     const firstname = parts[0];
@@ -27,7 +27,6 @@ function countStudents(path) {
 
   console.log(`Number of students in CS: ${groups.CS.length}. List: ${groups.CS.join(', ')}`);
   console.log(`Number of students in SWE: ${groups.SWE.length}. List: ${groups.SWE.join(', ')}`);
-
 }
 
 module.exports = countStudents;
