56c5c13c9a81bee2a1c72f3de664ad9f88ce51e1
Yugz29 <yannduzelier@gmail.com>
2026-01-05 11:18:02 +0100

    fix: ESLint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
-console.log('Welcome to Holberton School, what is your name?')
-process.stdin.on('data', data => {
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
 });
 
