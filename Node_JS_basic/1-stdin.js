1e9dc4ac633f3c6829d3ca2035645a831b17fffe
Yugz29 <yannduzelier@gmail.com>
2026-01-05 11:23:13 +0100

    fix: add process.stdin.isTTY and process.on('exit')

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,18 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
+
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
 });
 
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
+});
+
+process.on('exit', () => {
+  if (process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 });
