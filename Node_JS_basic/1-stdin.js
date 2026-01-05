316528e99f25919fd1a88611a1d50a726fdf625e
Yugz29 <yannduzelier@gmail.com>
2026-01-05 11:46:59 +0100

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  
+
   if (process.stdin.isTTY) {
-    process.exit();
+    process.stdin.pause();
   }
 });
 
