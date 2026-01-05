93145d386137c3df4b2ca7407f3fcc7a2e59f3b1
Yugz29 <yannduzelier@gmail.com>
2026-01-05 15:54:13 +0100

    add: Reading a file synchronously

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let students;
+
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').slice(1);
+    students = lines.filter(line => line !== '');
+    console.log(`Number of students: ${students.length}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const groups = {
+    CS: [],
+    SWE: []
+  };
+
+  for (let i = 0; i < students.length; i++) {
+    const line = students[i];
+    const parts = line.split(',');
+    const firstname = parts[0];
+    const field = parts[3];
+    groups[field].push(firstname);
+  }
+
+  console.log(`Number of students in CS: ${groups.CS.length}. List: ${groups.CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${groups.SWE.length}. List: ${groups.SWE.join(', ')}`);
+
+}
+
+module.exports = countStudents;
