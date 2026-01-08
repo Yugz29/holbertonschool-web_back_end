c2fc1a3de6cf4c27de627e899e242f5b5ffd57b0
Yugz29 <yannduzelier@gmail.com>
2026-01-08 18:30:28 +0100

    fix: writeHead remove

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,18 +4,22 @@ const countStudents = require('./3-read_file_async');
 const DATABASE = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
     countStudents(DATABASE)
       .then((result) => {
-        res.end(`This is the list of our students\n${result}`);
+        res.end(result);
       })
       .catch((error) => {
-        res.end(`This is the list of our students\n${error}`);
+        res.end(error.message);
       });
+  } else {
+    res.end();
   }
 });
 
