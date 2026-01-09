9eddc8d56a704072b42ee81dbcc637cd81f20e81
Yugz29 <yannduzelier@gmail.com>
2026-01-09 19:48:00 +0100

    fix: error message

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -12,25 +12,25 @@ class StudentsController {
         const lines = ['This is the list of our students'];
 
         fields.forEach((field) => {
-            const students = data[field];
-            const count = students.length;
-            const list = students.join(', ');
-            lines.push(`Number of students in ${field}: ${count}. List: ${list}`);
+          const students = data[field];
+          const count = students.length;
+          const list = students.join(', ');
+          lines.push(`Number of students in ${field}: ${count}. List: ${list}`);
         });
 
         const output = lines.join('\n');
-        
+
         res.status(200).send(output);
       })
-      .catch((error) => {
-        res.status(500).send(error.message);
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
       });
   }
 
   static getAllStudentsByMajor(req, res) {
     const { major } = req.params;
     if (major !== 'CS' && major !== 'SWE') {
-        return res.status(500).send('Major parameter must be CS or SWE');
+      return res.status(500).send('Major parameter must be CS or SWE');
     }
 
     readDatabase(DATABASE)
@@ -39,8 +39,8 @@ class StudentsController {
         const list = students.join(', ');
         res.status(200).send(`List: ${list}`);
       })
-      .catch((error) => {
-        res.status(500).send(error.message);
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
       });
   }
 }
