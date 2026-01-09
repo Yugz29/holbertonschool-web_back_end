df1557e946736296622c4e5a205e270e9ad6fdcc
Yugz29 <yannduzelier@gmail.com>
2026-01-09 15:13:53 +0100

    add: create a second app.get using async

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const DATABASE = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const result = await countStudents(DATABASE);
+    res.send(`This is the list of our students\n${result}`);
+  } catch (error) {
+    res.send(`This is the list of our students\n${error.message}`);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
